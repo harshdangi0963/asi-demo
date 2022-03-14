5d2835764e13d4196a013d52c7ce7607ff2dacf8
harshdangi0963 <harshdangi0963@gmail.com>
2022-03-14 12:52:34 +0530

    okok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import "./styles.css";
+import './styles.css'
 
 export default function App() {
   return (
     <div className="App">
       <img
         style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "2625px",
-          objectFit: "cover",
-          padding: 0,
-          margin: 0
+          display: "block",
+          width: '100vw',
+          height: 'auto',
+          objectFit: 'contain',
         }}
         alt=""
         src={
-          "https://firebasestorage.googleapis.com/v0/b/hyreme-f1997.appspot.com/o/big-min.jpg?alt=media&token=34776122-3219-488b-a520-70007227f3ad"
+          'https://firebasestorage.googleapis.com/v0/b/hyreme-f1997.appspot.com/o/big-min.jpg?alt=media&token=34776122-3219-488b-a520-70007227f3ad'
         }
       />
     </div>
-  );
+  )
 }
